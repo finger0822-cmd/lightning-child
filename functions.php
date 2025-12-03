439131a03876b09698aea22300af80dc0fc43903
蛛･ 驤ｴ譛ｨ <takeru.finger0822@gmail.com>
2025-12-03 17:55:58 +0900

    関数名の衝突を解消: theme_enqueue_stylesをlightning_child_enqueue_stylesに変更、不要なrequire_onceを削除

--- functions.php
+++ functions.php
@@ -3,12 +3,9 @@
  * Lightning Child Theme Functions
  */
 
-// 親テーマのfunctions.phpを読み込む
-require_once( get_template_directory() . '/functions.php' );
-
 // スタイルシートの読み込み
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'lightning_child_enqueue_styles' );
+function lightning_child_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 }
